77462e41f28b225251178c385aa82fae09900819
Aleksey Petsevich <aleksey.petsevich@rt.ru>
2024-09-28 09:50:17 +0500

    crdt

---
+++ crdt.js
@@ -0,0 +1,70 @@
+class CRDTTextEditor {
+    operation = [];
+    text = '';
+    clientId = '';
+
+    constructor(initText, clientId) {
+        this.text = initText;
+        this.operation = [];
+        this.clientId = clientId;
+    }
+
+    insert(char) {
+        const timestamp = new Date();
+        this.operation.push({ char, timestamp });
+        console.log(`Inserted "${char}" with timestamp ${timestamp} by ${this.clientId}`);
+    }
+
+    sync(remoteOps) {
+        this.operation.push(...remoteOps.filter((remoteOps) => {
+            for (const localOps of this.operation) {
+                if (remoteOps.char === localOps.char && remoteOps.timestamp === localOps.timestamp) {
+                    return false
+                }
+            }
+
+            return true;
+        }));
+    }
+
+    getText() {
+        console.log(`Initial text is "${this.text}"`);
+        return this.operation.sort((a, b) => a.timestamp - b.timestamp).reduce((acc, operation) => {
+            console.log(`Apply operation is char: ${operation.char}, timestamp: ${Number(operation.timestamp)}`);
+            return acc + operation.char;
+        }, [this.text]);
+    }
+
+    getOperation() {
+        return this.operation;
+    }
+}
+
+const initText = 'He'
+const editor1 = new CRDTTextEditor(initText, 'user1');
+const editor2 = new CRDTTextEditor(initText, 'user2');
+
+const sleep = (timeout) => new Promise((resolve) => {
+    setTimeout(() => resolve(), timeout);
+})
+
+async function test() {
+    editor1.insert('l');
+    await sleep(20);
+    editor2.insert('l');
+    await sleep(20);
+    editor1.insert('o');
+    await sleep(20);
+    editor2.insert('!');
+    await sleep(20);
+
+    editor1.sync(editor2.getOperation());
+
+    console.log(editor1.getText());
+
+    editor2.sync(editor1.getOperation());
+
+    console.log(editor2.getText());
+}
+
+test();
